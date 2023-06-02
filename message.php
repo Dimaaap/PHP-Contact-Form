e4853abd3758fcf29ed59e0b83d8b9d0e227fb5a
Dimaaap <procdima49@gmail.com>
2023-06-02 23:29:40 +0300

    A final version of form

--- message.php
+++ message.php
@@ -1,9 +1,3 @@
 <?php
 
-$name = $_POST["name"];
-$email = $_POST["email"];
-$phone = $_POST["phone"];
-$website = $_POST["website"];
-$message = $_POST["message"];
-
-echo $name;
+echo "This message was sent from PHP file";
